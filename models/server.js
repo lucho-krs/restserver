0440ab516ca195efdb6136ede45b4151ad088252
Luis Cereño Troncoso <luis.cereno.t@gmail.com>
2022-10-09 18:28:18 -0300

    Se crea conexion a la BD

--- models/server.js
+++ models/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const { dbConnection } = require('../database/config');
+
 class Server {
 
     constructor() {
@@ -8,11 +10,18 @@ class Server {
         this.port = process.env.PORT;
         this.userPath = '/api/users';
 
+        // Conectar a la base de datos
+        this.connectionDB();
+
         // Middleware
         this.middlewares();
 
         // Rutas de mi aplicacion
         this.routes();
+    }
+
+    async connectionDB() {
+        await dbConnection()
     }
 
     middlewares() { 
