33feea7a4d9681a302a80fff66e2e96a72bb4351
Luis Cereño Troncoso <luis.cereno.t@gmail.com>
2022-10-10 01:45:39 -0300

    Se valida role de usuario

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const Role = require('../models/role');
+
 const { validateData } = require('../middleware/validate-data');
+
 const { 
     getUser,
     putUser, 
@@ -15,7 +18,12 @@ router.post('/', [
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe ser mas de 6 caracteres').isLength({ min: 6 }),
     check('email', 'El correo no es valido').isEmail(),
-    check('role', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('role').custom( async(role = '') => {
+        const existRole = await Role.findOne({ role });
+        if (!existRole) {
+            throw new Error(`El rol ${ role } no está en la BD`)
+        }
+    }),
     validateData
 ], postUser);
 router.put('/:id', putUser);
